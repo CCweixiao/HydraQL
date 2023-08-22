298ee8a42d2b40e574ee0c6528b53691d7b7aeb9
leojie <leojie1314@gmail.com>
2023-08-22 22:52:39 +0800

    [Update] deploy配置和脚本更新

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,13 +7,13 @@ else
   echo "Please enter the HBase profile version you want to build."
   exit 1
 fi
-
+export GPG_TTY=$(tty)
 if [ "$version" == "1.2" ]; then
-  mvn clean deploy -Dmaven.test.skip=true -Pdeploy
+  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy
 elif [ "$version" == "1.4" ]; then
-  mvn clean deploy -Dmaven.test.skip=true -Pdeploy -Dhbase.profile=1.4
+  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhbase.profile=1.4 -projects hydraql-template,spring-boot-starter-hydraql
 elif [ "$version" == "2.2" ]; then
-  mvn clean deploy -Dmaven.test.skip=true -Pdeploy -Dhbase.profile=2.2
+  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhbase.profile=2.2 -projects hydraql-template,spring-boot-starter-hydraql
 else
   echo "The temporarily supported HBase profile versions are [1.2, 1.4, 2.2]"
 fi
