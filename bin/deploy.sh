33268d94709403e29dfd8faeba5244ce45dcd60c
leojie <leojie1314@gmail.com>
2024-03-24 12:13:57 +0800

    [Update] 1.0.1版本发布

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,9 +11,11 @@ export GPG_TTY=$(tty)
 if [ "$version" == "1.2" ]; then
   mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy
 elif [ "$version" == "1.4" ]; then
-  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhbase.profile=1.4 -projects hydraql-template,spring-boot-starter-hydraql
+  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=1.4 -Dhydraql.hbase.version=1.4.3 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
 elif [ "$version" == "2.2" ]; then
-  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhbase.profile=2.2 -projects hydraql-template,spring-boot-starter-hydraql
+  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.2 -Dhydraql.hbase.version=2.2.6 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
+elif [ "$version" == "2.5" ]; then
+  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.5 -Dhydraql.hbase.version=2.5.3 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
 else
   echo "The temporarily supported HBase profile versions are [1.2, 1.4, 2.2]"
 fi
