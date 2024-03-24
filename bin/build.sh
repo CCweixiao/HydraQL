3b9905bd1f86725cf2b293cec7a23a4cf4f77a61
leojie <leojie1314@gmail.com>
2024-03-24 11:26:15 +0800

    [Update] 编译配置优化

--- bin/build.sh
+++ bin/build.sh
@@ -11,11 +11,11 @@ fi
 if [ "$version" == "1.2" ]; then
   mvn clean install -Dmaven.test.skip=true
 elif [ "$version" == "1.4" ]; then
-  mvn clean install -Dmaven.test.skip=true -Dhbase.profile=1.4
+   mvn -T 4C clean -U install -Dmaven.test.skip=true -Dhydraql.hbase.profile=1.4 -Dhydraql.hbase.version=1.4.3
 elif [ "$version" == "2.2" ]; then
-  mvn clean install -Dmaven.test.skip=true -Dhbase.profile=2.2
+   mvn -T 4C clean -U install -Dmaven.test.skip=true -Dhydraql.hbase.profile=2.2 -Dhydraql.hbase.version=2.2.6
 elif [ "$version" == "2.5" ]; then
-  mvn clean install -Dmaven.test.skip=true -Dhbase.profile=2.5
+   mvn -T 4C clean -U install -Dmaven.test.skip=true -Dhydraql.hbase.profile=2.5 -Dhydraql.hbase.version=2.5.3
 else
   echo "The temporarily supported HBase profile versions are [1.2, 1.4, 2.2, 2.5]"
 fi
