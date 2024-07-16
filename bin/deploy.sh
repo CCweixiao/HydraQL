c71172705bec1c91abfe471b162ac77c2dfa168f
weixiao <leojie1314@gmail.com>
2024-07-16 22:55:46 +0800

    [Update] 发布1.0.2

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,13 +9,15 @@ else
 fi
 export GPG_TTY=$(tty)
 if [ "$version" == "1.2" ]; then
-  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy
+  mvn -T 4C clean -U deploy -Dmaven.test.skip=true -Pdeploy -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
 elif [ "$version" == "1.4" ]; then
-  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=1.4 -Dhydraql.hbase.version=1.4.3 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
+  mvn -T 4C clean -U deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=1.4 -Dhydraql.hbase.version=1.4.3 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
 elif [ "$version" == "2.2" ]; then
-  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.2 -Dhydraql.hbase.version=2.2.6 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
+  mvn -T 4C clean -U deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.2 -Dhydraql.hbase.version=2.2.7 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
 elif [ "$version" == "2.5" ]; then
-  mvn -T 4 clean deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.5 -Dhydraql.hbase.version=2.5.3 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
+  mvn -T 4C clean -U deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.5 -Dhydraql.hbase.version=2.5.3 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
+elif [ "$version" == "2.6" ]; then
+  mvn -T 4C clean -U deploy -Dmaven.test.skip=true -Pdeploy -Dhydraql.hbase.profile=2.6 -Dhydraql.hbase.version=2.6.0 -projects hydraql-template,hydraql-shell,spring-boot-starter-hydraql
 else
   echo "The temporarily supported HBase profile versions are [1.2, 1.4, 2.2]"
 fi
