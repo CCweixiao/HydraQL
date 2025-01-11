2e6fcda73a142f0873d40fb3900a2119dbc8b4a0
weixiao <leojie1314@gmail.com>
2025-01-11 15:04:33 +0800

    change version to 1.0.6-SNAPSHOT

--- bin/deploy-all.sh
+++ bin/deploy-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sh /Users/jielongping/other_project/HydraQL/bin/deploy.sh 1.2
+sh /Users/jielongping/other_project/HydraQL/bin/deploy.sh 1.2
 sh /Users/jielongping/other_project/HydraQL/bin/deploy.sh 1.4
 sh /Users/jielongping/other_project/HydraQL/bin/deploy.sh 2.2
 sh /Users/jielongping/other_project/HydraQL/bin/deploy.sh 2.5
